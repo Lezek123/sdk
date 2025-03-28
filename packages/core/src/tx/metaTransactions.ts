8cd46610fd7b0083028d8837fedde8c8c526f3a7
Lezek123 <leszek@jsgenesis.com>
2025-03-28 13:44:10 +0100

    Add more meta transactions

--- packages/core/src/tx/metaTransactions.ts
+++ packages/core/src/tx/metaTransactions.ts
@@ -1,15 +1,33 @@
 import '@joystream/types'
+import { u8aToU8a } from '@polkadot/util'
 import { ApiPromise } from '@polkadot/api'
-import { PalletMembershipBuyMembershipParameters } from '@polkadot/types/lookup'
+import {
+  PalletContentChannelCreationParametersRecord,
+  PalletContentChannelOwner,
+  PalletContentChannelUpdateParametersRecord,
+  PalletContentPermissionsContentActor,
+  PalletContentVideoCreationParametersRecord,
+  PalletContentVideoUpdateParametersRecord,
+  PalletMembershipBuyMembershipParameters,
+} from '@polkadot/types/lookup'
 import { AsSimple, metaToBytes } from '../chain/types'
 import {
+  AppAction,
+  ChannelMetadata,
+  ChannelModeratorRemarked,
   ChannelOwnerRemarked,
+  IAppAction,
+  IChannelMetadata,
+  IChannelModeratorRemarked,
   IChannelOwnerRemarked,
   IMemberRemarked,
   IMembershipMetadata,
+  IVideoMetadata,
   MemberRemarked,
   MembershipMetadata,
+  VideoMetadata,
 } from '@joystream/metadata-protobuf'
+import { AnyMetadataClass } from '@joystream/metadata-protobuf/types'
 
 export type WithReplaced<Original, Replacement> = Omit<
   Original,
@@ -28,11 +46,11 @@ export type MemberRemarkParams<Kind extends keyof IMemberRemarked> =
 export function memberRemarkMetaTx<Kind extends keyof IMemberRemarked>(
   api: ApiPromise,
   kind: Kind,
-  params: MemberRemarkParams<Kind>,
+  { memberId, ...params }: MemberRemarkParams<Kind>,
   payment?: [string | Uint8Array, number | bigint]
 ) {
   return api.tx.members.memberRemark(
-    params.memberId,
+    memberId,
     metaToBytes(MemberRemarked, {
       [kind]: params,
     }),
@@ -45,31 +63,159 @@ export type ChannelOwnerRemarkParams<Kind extends keyof IChannelOwnerRemarked> =
 
 export function channelOwnerRemarkMetaTx<
   Kind extends keyof IChannelOwnerRemarked,
->(api: ApiPromise, kind: Kind, params: ChannelOwnerRemarkParams<Kind>) {
+>(
+  api: ApiPromise,
+  kind: Kind,
+  { channelId, ...params }: ChannelOwnerRemarkParams<Kind>
+) {
   return api.tx.content.channelOwnerRemark(
-    params.channelId,
+    channelId,
     metaToBytes(ChannelOwnerRemarked, {
       [kind]: params,
     })
   )
 }
 
+export type ChannelModeratorRemarkParams<
+  Kind extends keyof IChannelModeratorRemarked,
+> = IChannelModeratorRemarked[Kind] & {
+  channelId: number | bigint
+  actor: AsSimple<PalletContentPermissionsContentActor>
+}
+
+export function channelModeratorRemarkMetaTx<
+  Kind extends keyof IChannelModeratorRemarked,
+>(
+  api: ApiPromise,
+  kind: Kind,
+  { actor, channelId, ...params }: ChannelModeratorRemarkParams<Kind>
+) {
+  return api.tx.content.channelAgentRemark(
+    actor,
+    channelId,
+    metaToBytes(ChannelModeratorRemarked, { [kind]: params })
+  )
+}
+
+export type BuyMembershipParams = MetaTxParams<
+  PalletMembershipBuyMembershipParameters,
+  { metadata?: IMembershipMetadata }
+>
+
+export type UpdateProfileParams = {
+  memberId: number | bigint
+  handle?: string
+  metadata?: IMembershipMetadata
+}
+
+export type AppAttributionParams = {
+  appId: string
+  signature: `0x${string}` | Uint8Array
+  metadata?: `0x${string}` | Uint8Array
+}
+
+export type CreateChannelParams = MetaTxParams<
+  PalletContentChannelCreationParametersRecord,
+  { meta?: IChannelMetadata }
+> & {
+  owner: AsSimple<PalletContentChannelOwner>
+  appAttribution?: AppAttributionParams
+}
+
+export type UpdateChannelParams = MetaTxParams<
+  PalletContentChannelUpdateParametersRecord,
+  { newMeta?: IChannelMetadata }
+> & {
+  actor: AsSimple<PalletContentPermissionsContentActor>
+  channelId: number | bigint
+}
+
+export type CreateVideoParams = MetaTxParams<
+  PalletContentVideoCreationParametersRecord,
+  { meta?: IVideoMetadata }
+> & {
+  actor: AsSimple<PalletContentPermissionsContentActor>
+  channelId: number | bigint
+  appAttribution?: AppAttributionParams
+}
+
+export type UpdateVideoParams = MetaTxParams<
+  PalletContentVideoUpdateParametersRecord,
+  { newMeta?: IVideoMetadata }
+> & {
+  actor: AsSimple<PalletContentPermissionsContentActor>
+  videoId: number | bigint
+}
+
+export function asAppAction<T>(
+  rawMetaClass: AnyMetadataClass<T>,
+  rawMeta: T | undefined,
+  { appId, signature, metadata }: AppAttributionParams
+): IAppAction {
+  return {
+    appId,
+    signature: u8aToU8a(signature),
+    metadata: u8aToU8a(metadata),
+    rawAction: rawMeta && rawMetaClass.encode(rawMeta).finish(),
+  }
+}
+
 export function metaTransactions(api: ApiPromise) {
   return {
     members: {
-      buyMembership(
-        params: MetaTxParams<
-          PalletMembershipBuyMembershipParameters,
-          { metadata: IMembershipMetadata }
-        >
-      ) {
+      buyMembership(params: BuyMembershipParams) {
         return api.tx.members.buyMembership({
           ...params,
-          metadata: metaToBytes(MembershipMetadata, params.metadata),
+          metadata:
+            params.metadata && metaToBytes(MembershipMetadata, params.metadata),
         })
+      },
+      updateProfile({ memberId, handle, metadata }: UpdateProfileParams) {
+        return api.tx.members.updateProfile(
+          memberId,
+          handle || null,
+          (metadata || null) && metaToBytes(MembershipMetadata, metadata)
+        )
       },
     },
     content: {
+      createChannel(params: CreateChannelParams) {
+        const { meta, appAttribution } = params
+        return api.tx.content.createChannel(params.owner, {
+          ...params,
+          meta: appAttribution
+            ? metaToBytes(
+                AppAction,
+                asAppAction(ChannelMetadata, meta, appAttribution)
+              )
+            : meta && metaToBytes(ChannelMetadata, meta),
+        })
+      },
+      updateChannel({ actor, channelId, ...params }: UpdateChannelParams) {
+        return api.tx.content.updateChannel(actor, channelId, {
+          ...params,
+          newMeta:
+            params.newMeta && metaToBytes(ChannelMetadata, params.newMeta),
+        })
+      },
+      createVideo({ actor, channelId, ...params }: CreateVideoParams) {
+        const { meta, appAttribution } = params
+        return api.tx.content.createVideo(actor, channelId, {
+          ...params,
+          meta: appAttribution
+            ? metaToBytes(
+                AppAction,
+                asAppAction(VideoMetadata, meta, appAttribution)
+              )
+            : meta && metaToBytes(VideoMetadata, meta),
+        })
+      },
+      updateVideo({ actor, videoId, ...params }: UpdateVideoParams) {
+        return api.tx.content.updateVideo(actor, videoId, {
+          ...params,
+          newMeta: params.newMeta && metaToBytes(VideoMetadata, params.newMeta),
+        })
+      },
       reactVideo(params: MemberRemarkParams<'reactVideo'>) {
         return memberRemarkMetaTx(api, 'reactVideo', params)
       },
@@ -122,8 +268,15 @@ export function metaTransactions(api: ApiPromise) {
       ) {
         return channelOwnerRemarkMetaTx(api, 'videoReactionsPreference', params)
       },
-      moderateComment(params: ChannelOwnerRemarkParams<'moderateComment'>) {
+      moderateCommentAsOwner(
+        params: ChannelOwnerRemarkParams<'moderateComment'>
+      ) {
         return channelOwnerRemarkMetaTx(api, 'moderateComment', params)
+      },
+      moderateCommentAsModerator(
+        params: ChannelModeratorRemarkParams<'moderateComment'>
+      ) {
+        return channelModeratorRemarkMetaTx(api, 'moderateComment', params)
       },
     },
   } as const
